0924890aa4388eb4982ec9cdacdeb9b617f35d14
devarturobl <4rthurbl00@gmail.com>
2025-09-15 10:53:45 -0600

    Diseño del Fondo

---
+++ Semana3/flutter_application_2/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFF4B74FF), // Blues
+                Color(0xFF7D4BFF), // Green
+              ],
+            ),
+          ),
+          child: Center(
+            child: Text(
+              'Unidades de Conversión',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
