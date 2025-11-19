abff6e5f9c20beb4a4c6eee2e5c274d5b66e7c56
devarturobl <4rthurbl00@gmail.com>
2025-11-19 10:10:06 -0600

    Semana13 Act2

--- Semana13/supabaseapp/lib/main.dart
+++ Semana13/supabaseapp/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:supabaseapp/note_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://ddcjhhdntfdjnpbfcjrw.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkY2poaGRudGZkam5wYmZjanJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM1NjEyOTUsImV4cCI6MjA3OTEzNzI5NX0.Ld0rwYhb0t7XLuGqiDbL2FScBcWb4swNq5MTO1ESTVs',
+  );
   runApp(const Myapp());
 }
 
@@ -13,7 +20,7 @@ class Myapp extends StatelessWidget {
     return MaterialApp(
       title: 'Supabase APP',
       theme: ThemeData.dark(),
-      home: NotePage()
+      home: NotePage(),
     );
   }
 }
